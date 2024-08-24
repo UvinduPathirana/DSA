b8bdaa41454c71b928b1adc79d665eff12951ba9
Uvindu Pathirana <65161616+UvinduPathirana@users.noreply.github.com>
2024-08-24 15:33:55 +0530

    sliding window

--- problem-solving-patterns/multiple_pointers.js
+++ problem-solving-patterns/multiple_pointers.js
@@ -30,3 +30,19 @@ function sumZero(arr){
 
 const response = sumZero([1, 5, 6, -7, -5, 2])
 console.log(response)
+
+
+// multiple pointers - count unique values challenge
+function unique(arr){
+    var i = 0;
+    for(let j = 0; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+        // console.log(i, j)
+    }
+    return i + 1;
+}
+
+console.log(unique([1,1,1,1,2,3]))
