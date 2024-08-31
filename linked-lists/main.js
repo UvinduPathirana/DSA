462c81d6e04781194e8aee8a8e955b9314b1dd4c
Uvindu Pathirana <65161616+UvinduPathirana@users.noreply.github.com>
2024-08-31 11:11:00 +0530

    reversing a linked list

--- linked-lists/main.js
+++ linked-lists/main.js
@@ -113,6 +113,21 @@ class LinkedList {
         this.length--
         return temp
     }
+
+    reverse(){
+        let temp = this.head
+        this.head = this.tail
+        this.tail = this.head
+        let next = temp.next
+        let prev = null
+        for(let i =0; i < this.length; i++){
+            next = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next
+        }
+        return this
+    }
 }
 
 let myLinkedList = new LinkedList(4)
