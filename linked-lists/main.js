fd888ea32e920b84e819ae1c08f0ba55c768140b
Uvindu Pathirana <65161616+UvinduPathirana@users.noreply.github.com>
2024-08-31 11:06:15 +0530

    linked list complete

--- linked-lists/main.js
+++ linked-lists/main.js
@@ -56,6 +56,63 @@ class LinkedList {
         this.length++
         return this
     }
+    shift(){
+        if(!this.head) return undefined
+        let temp = this.head
+        this.head = this.head.next
+        this.length--
+        if(this.length == 0){
+            this.tail = null
+        }
+        temp.next = null
+        return temp
+    }
+
+    get(index){
+        if(index < 0 || index >= this.length){
+            return undefined
+        }
+        let temp = this.head
+        for(let i = 0; i < index; i++) {
+            temp = temp.next
+        }
+        return temp
+    }
+    set(index, value){
+        let temp = this.get(index)
+        if(temp){
+            temp.value = value
+            return true
+        }
+        return false;
+    }
+    insert(index, value){
+        if(index === 0) return this.unshift(value)
+        if(index === this.length) return this.push(value)
+        if(index < 0 || index > this.length) return false
+
+        const newNode = new Node(value)
+        const temp = this.get(index - 1)
+
+        newNode.next = temp.next
+        temp.next = newNode
+        this.length++ 
+        return true
+    }
+
+    remove(index){
+        if(index === 0) return this.shift()
+        if(index === this.length -1) return this.pop()
+        if(index < 0 || index >= this.length) return undefined
+
+        const before = this.get(index - 1)
+        const temp = before.next
+
+        before.next = temp.next
+        temp.next = null
+        this.length--
+        return temp
+    }
 }
 
 let myLinkedList = new LinkedList(4)
