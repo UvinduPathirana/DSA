8c9164418710187aef52f5809394d18d9ee8cd69
Uvindu Pathirana <65161616+UvinduPathirana@users.noreply.github.com>
2024-09-30 21:56:35 +0530

    changes

--- linked-lists/duplicate.js
+++ linked-lists/duplicate.js
@@ -5,7 +5,7 @@ class Node {
     }
 }
 
-class LinkedList {
+class LinkedList{
     constructor(value){
         const newNode = new Node(value)
         this.head = newNode
@@ -22,7 +22,7 @@ class LinkedList {
             this.tail.next = newNode
             this.tail = newNode
         }
-        this.length++
+        this.length ++
         return this
     }
 
@@ -41,79 +41,6 @@ class LinkedList {
             this.head = null
             this.tail = null
         }
-        return temp
-    }
-
-    unshift(value){
-        const newNode = new Node(value)
-        if(!this.head){
-            this.head = newNode
-            this.tail = newNode
-        } else {
-            newNode.next = this.head
-            this.head = newNode
-        }
-        this.length ++ 
-        return this
-    }
-
-    shift(){
-        if(!this.head) return undefined
-        let temp = this.head
-        this.head = this.head.next 
-        this.length--
-        if(this.lenght == 0){
-            this.tail = null
-        }
-        temp.next = null
-        return temp
-    }
-
-    get(index){
-        if(index < 0 || index >= this.length){
-            return undefined
-        }
-        let temp = this.head
-        for(let i = 0; i < index; i++){
-            temp = temp.next
-        }
-        return temp
-    }
-
-    set(index, value){
-        let temp = this.get(index)
-        if(temp){
-            temp.value = value
-            return true
-        }
-        return false
-    }
-
-    insert(index, value){
-        if(index === 0) return this.unshift(value)
-        if(index === this.length) return this.push(value)
-        if(index < 0 || index > this.length) return false
-
-        const newNode = new Node(value)
-        const temp = this.get(index - 1)
-
-        newNode.next = temp.next
-        temp.next = newNode
-        this.length++
-        return true
-    }
-
-    remove(index){
-        if(index === 0) return this.shift()
-        if(index === this.length -1) return this.pop()
-        if(index < 0 || index >= this.length) return undefined
-
-        const before = this.get(index - 1)
-        const temp = before.next
-
-        before.next = temp.next
-        temp.next = null
-        this.length--
         return temp
     }
 }
