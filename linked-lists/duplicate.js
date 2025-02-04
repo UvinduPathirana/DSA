ab26f4d86429cf19c8eab963a0676d97c6ea3877
Uvindu Pathirana <65161616+UvinduPathirana@users.noreply.github.com>
2025-02-04 15:21:34 +0530

    add frequency counter function and enhance linked list methods

--- linked-lists/duplicate.js
+++ linked-lists/duplicate.js
@@ -5,11 +5,12 @@ class Node {
     }
 }
 
+
 class LinkedList{
-    constructor(value){
+    constructor(value) {
         const newNode = new Node(value)
         this.head = newNode
-        this.tail = newNode
+        this.tail = this.head
         this.length = 1
     }
 
@@ -17,12 +18,12 @@ class LinkedList{
         const newNode = new Node(value)
         if(!this.head){
             this.head = newNode
-            this.tail = newNode
-        } else {
+            this.tail = newNode;
+        } else{
             this.tail.next = newNode
             this.tail = newNode
         }
-        this.length ++
+        this.length++
         return this
     }
 
@@ -41,6 +42,95 @@ class LinkedList{
             this.head = null
             this.tail = null
         }
+        return value
+    }
+
+    unshift(value){
+        const newNode = new Node(value)
+        if(!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+    
+    shift(){
+        if(!this.head) return undefined
+        let temp = this.head
+        this.head = this.head.next
+        this.length--
+        if(this.length==0){
+            this.tail = null
+        }
+        temp.next = null
         return temp
+    }
+
+    get(index){
+        if(index < 0 || index >= this.length){
+            return undefined
+        }
+        let temp = this.head
+        for(let i = 0; i < index; i++){
+            temp = temp.next
+        }
+        return temp
+    }
+
+    set(index, value){
+        let temp = this.get(index)
+        if(temp){
+            temp.value = value
+            return true
+        }
+        return false
+    }
+
+    insert(index, value){
+        if(index===0) return this.unshift(value)
+        if(index === this.length) return this.push(value)
+        if(index < 0 || index > this.length) return false
+
+
+        const newNode = new Node(value)
+        const temp = this.get(index - 1)
+
+        newNode.next = temp.next
+        temp.next = newNode
+        this.length++
+        return true
+    }
+
+    remove(index){
+        if(index===0) return this.unshift(value)
+        if(index === this.length -1) return this.push(value)
+        if(index < 0 || index > this.length) return undefined
+        
+        const before = this.get(index - 1)
+        const temp = before.next
+
+        before.next = temp.next
+        temp.next = null
+        this.length--
+        return temp
+    }
+    
+    reverse(){
+        let temp = this.head
+        this.head = this.tail
+        this.tail = this.head
+        let next = temp.next
+        let prev = null
+        for(let i =0; i < this.length; i++){
+            next = temp.next
+            temp.next = prev
+            prev = temp
+            temp = next
+        }
+        return this
     }
 }
